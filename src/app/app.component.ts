f6156ca3f676f20677377fdfea82bd4b49ba45f2
kunglaw <alhusna901@gmail.com>
2021-02-08 07:13:20 +0700

    two ways binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'belajar-angular';
+
+  firstName:string = "Aries Dimas"
+  lastName:string = "Yudhistira"
+
+  imageUrl="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
+
+  constructor() {
+    console.log("hello world")
+
+    this.title = "Belajar Angular"
+  }
+
+  hello(value:string) { 
+    console.log(value)
+  }
 }
